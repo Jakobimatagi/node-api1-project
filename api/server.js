2ed6e69f58cb5a30d3d2e0c8967b0f5bc0c9f46e
Jakobi Matagi <jkmatagi@hotmail.com>
2021-04-07 19:28:19 -0600

    C:/Program Files/Git/users gets the user data and displays it

--- api/server.js
+++ api/server.js
@@ -1,3 +1,33 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+const db = require("./users/model.js")
+
+
+
+server.get("/", (req, res) => {
+    res.json({message: "hello"})
+})
+
+server.get("/users", (req, res) => {
+    db.find()
+        .then(
+         // the promise response here is the users array
+         function (x) {
+             console.log(x)
+             res.json(x)
+         }
+        
+        )
+        .catch({
+         // no info is retrieved
+         function (err) {
+             console.log(err)
+         }
+        })
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
