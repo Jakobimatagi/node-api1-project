9012d0c1d11929d3335b292bd4c4684898057b06
Jakobi Matagi <jkmatagi@hotmail.com>
2021-04-07 23:34:08 -0600

    added more of the structure for each mvp

--- api/server.js
+++ api/server.js
@@ -13,6 +13,8 @@ server.get("/", (req, res) => {
     res.json({message: "hello"})
 })
 
+
+// Get Users
 server.get("/users", (req, res) => {
     db.find()
         .then(
@@ -30,4 +32,62 @@ server.get("/users", (req, res) => {
          }
         })
 })
+
+
+// Get User by id
+server.get("/user/:id", (req, res) =>{
+    db.findById()
+    .then(
+        
+        function (x) {
+            
+            console.log(x)
+            res.json(x)
+        }
+    )
+    .catch(
+        function (err) {
+            console.log(err)
+        }
+    )
+})
+
+// Create User
+
+// server.post("/users", (req, res)=>{
+//     db.insert()
+//     .then(
+//         // the promise response here is the new user
+//         function (x) {
+//             res.json(x)
+//         }
+//     )
+//     .catch(
+//         // error
+//         function (err) {
+//             console.log(err)
+//         }
+//     )
+// })
+
+
+// Delete User by id
+
+server.delete("/users/:id", (req, res) => {
+    const id = res.id
+    db.remove(id)
+})
+
+
+// Update User
+
+server.put("/users/:id", (req, res) =>{
+    db.update()
+})
+
+
+
+
+
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
